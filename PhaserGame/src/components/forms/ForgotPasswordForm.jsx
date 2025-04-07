f9fc692a03677442dbdaa27a146b4f284fab231b
codedumpforArmino <73728192+codedumpforArmino@users.noreply.github.com>
2025-04-08 06:33:02 +0800

    Initial Commit
    Disabled index.css

--- PhaserGame/src/components/forms/ForgotPasswordForm.jsx
+++ PhaserGame/src/components/forms/ForgotPasswordForm.jsx
@@ -4,14 +4,34 @@ class ForgotPasswordForm extends Component{
     render(){
         return(
             <div>
-                <input
-                type="email"
-                name="email"
-                // value={this.props.email}
-                onChange={this.props.onChange}
-                placeholder="Email"
-                />
-                <button onClick={this.props.onForgotPassword}>Confirm</button>
+                <header>
+                    <nav>
+                    <div class="nav-items">
+                        <a href="#">Home</a>
+                        <a href="#">Users</a>
+                        <a href="#">Ranking</a>
+                        <a href="#">Topics</a>
+                        <a href="#">Settings</a>
+                    </div>
+                    </nav>
+                </header>
+
+                <div id="body-container">
+                    <div id="body-section">
+                        <h2>Forgot Password?</h2>
+                        <p>Enter the email address you registered this website, and we will send you an email message with password reset information. </p>
+                        <input
+                        type="email"
+                        name="email"
+                        // value={this.props.email}
+                        onChange={this.props.onChange}
+                        placeholder="Email"
+                        />
+
+                        <button onClick={this.props.onForgotPassword}>Confirm</button>
+                    </div>
+                </div>
+                
             </div>
         );
     }
--- PhaserGame/src/components/forms/LoginForm.jsx
+++ PhaserGame/src/components/forms/LoginForm.jsx
@@ -4,24 +4,43 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <input
-          type="email"
-          name="email"
-          value={this.props.email}
-          onChange={this.props.onChange}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          name="password"
-          // value={this.props.password}
-          onChange={this.props.onChange}
-          placeholder="Password"
-        />
-        <button onClick={this.props.onLogin}>Login</button>
-        <button onClick={this.props.onForgotPassword}>
-          Forgot Password
-        </button>
+        <header>
+            <nav>
+            <div class="nav-items">
+                <a href="#">Home</a>
+                <a href="#">Users</a>
+                <a href="#">Ranking</a>
+                <a href="#">Topics</a>
+                <a href="#">Settings</a>
+            </div>
+            </nav>
+        </header>
+
+        <div id="body-container">
+          <div id="body-section">
+          <h2>Sign In</h2>
+            <div class="input-group">
+              <label for="username">Username</label>
+              <input type="email" name="email" value={this.props.email} onChange={this.props.onChange} placeholder="Email"/>
+            </div>
+
+            <div class="input-group">
+              <label for="password">Password</label>
+              <input type="password" name="password"// value={this.props.password}
+                onChange={this.props.onChange}
+                placeholder="Password"
+              />
+            </div>
+
+            <a href="">New User?</a>
+            <a href="">Forgot Password?</a>
+            <button onClick={this.props.onLogin}>Login</button>
+            <button onClick={this.props.onForgotPassword}>Forgot Password</button>
+            </div>
+          </div>
+          
+        
+       
         {this.props.error && <p>Error: {this.props.error}</p>}
       </div>
     );
