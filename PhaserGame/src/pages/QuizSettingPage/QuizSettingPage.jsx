fe065f1c9fd2861edf0796af7a8d553d945e0a64
zyle-geralde <zylgerdlp19@gmail.com>
2025-05-16 03:04:09 +0800

    delete event function

--- PhaserGame/src/pages/QuizSettingPage/QuizSettingPage.jsx
+++ PhaserGame/src/pages/QuizSettingPage/QuizSettingPage.jsx
@@ -8,11 +8,12 @@ export default function QuizSettingPage() {
   //check if it is "/addQuiz" Route
   const location = useLocation();
   const isAddQuizRoute = location.pathname === '/addQuiz';
+  const alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
   const [questions, setQuestions] = useState([
-    { question: '', numerator: '', denominator: '', probability: '' ,isNew: false },
-    { question: '', numerator: '', denominator: '',probability: '',isNew: false },
-    { question: '', numerator: '', denominator: '', probability: '',isNew: false },
+    { question: '', numerator: '', denominator: '', probability: '', events: ["1 or 2 when dice is rolled", "hellow b"], isNew: false },
+    { question: '', numerator: '', denominator: '', probability: '', events: ["3 or 4 when dice is rolled", "hellow c", "hellow cc"], isNew: false },
+    { question: '', numerator: '', denominator: '', probability: '', events: ["5 or 6 when dice is rolled", "hellow d", "hellow cd", "hellow ddd"], isNew: false },
   ]);
 
   const [showConfirm, setShowConfirm] = useState(false);
@@ -21,7 +22,7 @@ export default function QuizSettingPage() {
 
   const handleAddQuestion = () => {
     setQuestions([
-      { question: '', numerator: '', denominator: '',probability: '', isNew: true },
+      { question: '', numerator: '', denominator: '', probability: '', isNew: true },
       ...questions
     ]);
   };
@@ -59,17 +60,32 @@ export default function QuizSettingPage() {
     setTargetIndex(null);
   };
 
-  const handleChange = (index, field, value) => {
+  const handleChange = (index, field, value, indxx = null) => {
     const updatedQuestions = [...questions];
-    updatedQuestions[index][field] = value;
+    if (field == "events") {
+      updatedQuestions[index][field][indxx] = value;
+    }
+    else {
+      updatedQuestions[index][field] = value;
+    }
+
     setQuestions(updatedQuestions);
+    console.log(updatedQuestions)
   };
 
   const handleSave = (index) => {
     const updatedQuestions = [...questions];
     updatedQuestions[index].isNew = false;
     setQuestions(updatedQuestions);
+    
   };
+
+  const handleEventDeletion = (index, field, indxx)=>{
+    const updatedQuestions = [...questions];
+
+    updatedQuestions[index][field].splice(indxx, 1)
+    setQuestions(updatedQuestions)
+  }
 
   return (
     <>
@@ -85,24 +101,24 @@ export default function QuizSettingPage() {
           {/*contains name of the quiz, the difficuty, and the time*/}
           {isAddQuizRoute &&
             <div className="d-flex flex-row">
-            <div className="d-flex flex-row align-items-center">
-              <label className="me-2 text-white fw-bold">Name</label>
-              <input className="form-control w-100" />
-            </div>
+              <div className="d-flex flex-row align-items-center">
+                <label className="me-2 text-white fw-bold">Name</label>
+                <input className="form-control w-100" />
+              </div>
 
-            <div className="d-flex flex-row align-items-center">
-              <label className="me-2 text-white fw-bold">Difficulty</label>
-              <select className="form-select w-100">
-                <option value="beginner">Beginner</option>
-                <option value="intermediate">Intermediate</option>
-                <option value="advanced">Advanced</option>
-              </select>
-            </div>
+              <div className="d-flex flex-row align-items-center">
+                <label className="me-2 text-white fw-bold">Difficulty</label>
+                <select className="form-select w-100">
+                  <option value="beginner">Beginner</option>
+                  <option value="intermediate">Intermediate</option>
+                  <option value="advanced">Advanced</option>
+                </select>
+              </div>
 
-            <div className="d-flex flex-row align-items-center">
-              <label className="me-2 text-white fw-bold">Time (minutes)</label>
-              <input type="number" className="form-control w-25" placeholder="ex. 30" min="1" />
-            </div>
+              <div className="d-flex flex-row align-items-center">
+                <label className="me-2 text-white fw-bold">Time (minutes)</label>
+                <input type="number" className="form-control w-25" placeholder="ex. 30" min="1" />
+              </div>
             </div>
           }
         </div>
@@ -128,25 +144,44 @@ export default function QuizSettingPage() {
               {questions.map((q, index) => (
                 <tr key={index}>
                   <td className="quiz-settings-td">
-                    <div className='d-flex flex-column align-items-start fw-bold' style={{fontSize:"12px"}}>
+                    <div className='d-flex flex-column align-items-start fw-bold' style={{ fontSize: "12px" }}>
                       <div>Description: </div>
-                    <input
-                      type="text"
-                      className="quiz-settings-input"
-                      placeholder="Enter question"
-                      value={q.question}
-                      onChange={(e) => handleChange(index, 'question', e.target.value)}
-                    />
-                      
-                    <div>
-                        
-                    </div>  
-                      
-                    <div className="d-flex flex-row align-items-center mt-2">
-                      <i className="fas fa-plus-circle"></i>
-                      <div className="ms-2" style={{cursor:"pointer"}}>Add Event</div> 
-                    </div>
-                     
+                      <input
+                        type="text"
+                        className="quiz-settings-input"
+                        placeholder="Enter question"
+                        value={q.question}
+                        onChange={(e) => handleChange(index, 'question', e.target.value)}
+                      />
+
+
+                      { /*For Events*/}
+                      <div className='w-75 ms-3'>
+                        {q.events.map((js, indx) => (
+                          <div className="d-flex flex-row align-items-center mt-2" key={indx}>
+                            <div className="me-2 fw-bold">{alphanum[indx]}</div>
+                            <div className='me-2 fw-bold'>=</div>
+                            <input
+                              type="text"
+                              className="form-control"
+                              value={js}
+                              onChange={(e) => handleChange(index, "events", e.target.value, indx)}
+                            />
+                            <i
+                              className="fas fa-trash-alt text-danger cursor-pointer ms-2"
+                              style={{ cursor: 'pointer' }}
+                              onClick={(e) => handleEventDeletion(index,"events",indx)}
+                            ></i>
+                          </div>
+
+                        ))}
+                      </div>
+
+                      <div className="d-flex flex-row align-items-center mt-2 ms-3" >
+                        <i className="fas fa-plus-circle"></i>
+                        <div className="ms-2" style={{ cursor: "pointer" }}>Add Event</div>
+                      </div>
+
 
 
                     </div>
@@ -155,16 +190,16 @@ export default function QuizSettingPage() {
                     <div className='d-flex flex-row fs-3'>
                       <div className="me-2">P(</div>
                       <input
-                      type="text"
-                      className="quiz-settings-input w-100"
-                      value={q.probability}
-                      placeholder='(A | B)'
-                      onChange={(e) => handleChange(index, 'probability', e.target.value)}
+                        type="text"
+                        className="quiz-settings-input w-100"
+                        value={q.probability}
+                        placeholder='(A | B)'
+                        onChange={(e) => handleChange(index, 'probability', e.target.value)}
                       />
                       <div className="ms-2">)</div>
                     </div>
                   </td>
-                  <td className="quiz-settings-td" style={{width:"150px"}}>
+                  <td className="quiz-settings-td" style={{ width: "150px" }}>
                     <input
                       type="number"
                       className="quiz-settings-input"
@@ -172,7 +207,7 @@ export default function QuizSettingPage() {
                       onChange={(e) => handleChange(index, 'numerator', e.target.value)}
                     />
                   </td>
-                  <td className="quiz-settings-td" style={{width:"150px"}}>
+                  <td className="quiz-settings-td" style={{ width: "150px" }}>
                     <input
                       type="number"
                       className="quiz-settings-input"
@@ -204,8 +239,8 @@ export default function QuizSettingPage() {
             <p>
               {confirmType === "deleteQuiz"
                 ? "This will permanently delete the quiz and all questions."
-                : confirmType === "cancelQuiz"? "This will delete your unsaved work"
-                : "This will delete the selected question."}
+                : confirmType === "cancelQuiz" ? "This will delete your unsaved work"
+                  : "This will delete the selected question."}
             </p>
             <div className="form-buttons">
               <button onClick={handleConfirmDelete}>Confirm</button>
