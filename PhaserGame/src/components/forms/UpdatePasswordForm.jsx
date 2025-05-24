ec2e71ce54a342a9305af9b0046cd547b5d65e41
Subatomi <laderatheo@gmail.com>
2025-05-25 02:05:56 +0800

    UpdatePasswordForm

--- PhaserGame/src/components/forms/UpdatePasswordForm.jsx
+++ PhaserGame/src/components/forms/UpdatePasswordForm.jsx
@@ -1,15 +1,30 @@
+import HomeNavbar from "../navbar/HomeNavbar";
+
+import "./UpdatePasswordForm.css";
+
 function UpdatePasswordForm({ onChange, onUpdate, error }) {
   return (
-    <div>
-      <input
-        type="password"
-        name="password"
-        onChange={onChange}
-        placeholder="New Password"
-      />
-      <button onClick={onUpdate}>Confirm</button>
-      {error && <p>Error: {error}</p>}
-    </div>
+    <>
+      <HomeNavbar/>
+      <div className="update-pass-page-wrapper">
+        <div className="update-pass-container">
+          <h2>Enter New Password</h2>
+
+          <div className="input-group">
+            <label htmlFor="userName">New Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={onChange}
+              placeholder="New Password"
+            />
+            <button onClick={onUpdate}>Confirm</button>
+          </div>
+          {error && <p>Error: {error}</p>}
+        </div>
+        
+      </div>
+    </>
   );
 }
 
