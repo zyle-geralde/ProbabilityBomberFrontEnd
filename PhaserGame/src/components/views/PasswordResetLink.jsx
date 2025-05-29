8b0a018c12639732336430d9da514b6b31444fbf
Subatomi <laderatheo@gmail.com>
2025-05-30 00:21:48 +0800

    Update Forgot Password UI and Design

--- PhaserGame/src/components/views/PasswordResetLink.jsx
+++ PhaserGame/src/components/views/PasswordResetLink.jsx
@@ -1,14 +1,30 @@
+import "../forms/UpdatePasswordForm.css";
+
 function PasswordResetLinkComponent({ userData, onLogin }) {
-  return(
-  <div>
-      <a href={userData} target="_blank" rel="noopener noreferrer">
-        Reset Password Link
-      </a>
-      <br/>
-      <button onClick={onLogin}>
-      Login
-      </button>
-  </div>
+  const handleOpenResetLink = () => {
+    if (userData) {
+      window.open(userData, "_blank", "noopener,noreferrer");
+    }
+  };
+
+  return (
+    <div className="update-pass-page-wrapper">
+      <div className="update-pass-container">
+        {/* Reset Password as a button that opens the link in new tab */}
+        <button onClick={handleOpenResetLink}>
+          Reset Password Link
+        </button>
+
+        {/* Login as a styled link */}
+        <a href="#" onClick={(e) => {
+          e.preventDefault();
+          onLogin();
+        }}>
+          Login
+        </a>
+      </div>
+    </div>
   );
 }
+
 export default PasswordResetLinkComponent;
