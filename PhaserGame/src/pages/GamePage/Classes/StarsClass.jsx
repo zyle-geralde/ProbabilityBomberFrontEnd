86ce45146ca79f8cb9c690310225b8fb9549415a
zyle-geralde <zylgerdlp19@gmail.com>
2025-09-27 11:41:47 +0800

    PhaserGameV2

--- PhaserGame/src/pages/GamePage/Classes/StarsClass.jsx
+++ PhaserGame/src/pages/GamePage/Classes/StarsClass.jsx
@@ -1,4 +1,4 @@
-class Stars{
+class Stars {
     constructor(self) {
         this.self = self
         this.size = 32
@@ -8,27 +8,48 @@ class Stars{
         this.self.star2 = this.self.add.image(1192, 70, "halfStar").setDisplaySize(this.size, this.size);
         this.self.star3 = this.self.add.image(1234, 90, "halfStar").setDisplaySize(this.size, this.size);
     }
+
     changeStars() {
+        const animateStar = (starRef, x, y) => {
+            if (!starRef) return;
+
+            // Fade out the half star
+            this.self.tweens.add({
+                targets: starRef,
+                alpha: 0,
+                duration: 300,
+                ease: "Power1",
+                onComplete: () => {
+                    starRef.destroy();
+
+                    // Create new full star at the same position
+                    let fullStar = this.self.add.image(x, y, "fullStar")
+                        .setDisplaySize(0, 0)  // start from 0
+                        .setAlpha(0);
+
+                    // Animate display size to desired size
+                    this.self.tweens.add({
+                        targets: fullStar,
+                        alpha: 1,
+                        displayWidth: this.size,
+                        displayHeight: this.size,
+                        duration: 300,
+                        ease: "Power1"
+                    });
+
+                    return fullStar;
+                }
+            });
+        };
+
+        if (this.self.pointCount == 1) {
+            this.self.star1 = animateStar(this.self.star1, 1150, 90) || this.self.star1;
+        }
         if (this.self.pointCount == 2) {
-            if (this.self.star1) {
-                this.self.star1.destroy()
-                this.self.star1 = null
-                this.self.star1 = this.self.add.image(1150, 90, "fullStar").setDisplaySize(this.size, this.size);
-            }
+            this.self.star2 = animateStar(this.self.star2, 1192, 70) || this.self.star2;
         }
-        if (this.self.pointCount == 4) {
-            if (this.self.star2) {
-                this.self.star2.destroy()
-                this.self.star2 = null
-                this.self.star2 =this.self.add.image(1192, 70, "fullStar").setDisplaySize(this.size, this.size);
-            }
-        }
-        if (this.self.pointCount == 6) {
-            if (this.self.star3) {
-                this.self.star3.destroy()
-                this.self.star3 = null
-                this.self.star3 = this.self.add.image(1234, 90, "fullStar").setDisplaySize(this.size, this.size);
-            }
+        if (this.self.pointCount == 3) {
+            this.self.star3 = animateStar(this.self.star3, 1234, 90) || this.self.star3;
         }
     }
 }
