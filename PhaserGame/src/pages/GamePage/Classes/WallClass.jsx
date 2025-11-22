717905450a99bd882f049e6fdd20d4d305f0b88a
zyle-geralde <zylgerdlp19@gmail.com>
2025-11-22 22:41:05 +0800

    adjustment on stage 3 walls

--- PhaserGame/src/pages/GamePage/Classes/WallClass.jsx
+++ PhaserGame/src/pages/GamePage/Classes/WallClass.jsx
@@ -181,10 +181,10 @@ class Wall {
             { "col": 8, "row": 4 }]
         }
         else if (this.self.stage == 3) {
-             this.insideWallDimension = [ { "col": 5, "row": 2 }, { "col": 2, "row": 3 }, { "col": 3, "row": 5 },
-             { "col": 5, "row": 6 }, { "col": 10, "row": 3 }, { "col": 7, "row": 2 },
-            { "col": 9, "row": 5 }, { "col": 7, "row": 6 }, { "col": 5, "row": 4 },
-            { "col": 7, "row": 4 }]
+             this.insideWallDimension = [ { "col": 6, "row": 2 }, { "col": 3, "row": 3 }, { "col": 4, "row": 5 },
+             { "col": 6, "row": 6 }, { "col": 11, "row": 3 }, { "col": 8, "row": 2 },
+            { "col": 10, "row": 5 }, { "col": 8, "row": 6 }, { "col": 6, "row": 4 },
+            { "col": 8, "row": 4 }]
         }
     }
     createLeftWalls() {
